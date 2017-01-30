a19624cf44d7df1da4480d98b0d74e62d77e2934
Elena Santana <fanelenan7@gmail.com>
2017-01-30 17:07:16 -0500

    starting over

--- script.js
+++ script.js
@@ -1,33 +1,24 @@
-// $(function() {
-//   $( "#sortable1, #sortable2, #sortable3" ).sortable({
-//     connectWith: ".connectedSortable"}).disableSelection()
-// })
 var rings = ["#ring1", "#ring2", "#ring3"]
-// var towers
 
-$(".peg").mousedown(function() {
-  if
-  ($(this).children().eq(0))
+$(".ring").click(function() {
+  if ($(this).eq(0)) {
+    console.log($(this))
   // ($(".this").children().hasClass("div:first-child"))
-   {
-    // console.log($(this))
-  $(function() {
-    // $(this).children(".ring").eq(0)
-    $(".ring").eq(0).draggable({grid: [20, 20]});
-  })} else {
+    $(".ring").eq(0).css("background-color", "rebeccapurple")
+  } else {
     return
     }
   })
-$(".peg").mouseup(function() {
-  if (rings.indexOf($(this).children().eq(0).attr("id")) < rings.indexOf($(this).children().first().attr("id")) || $(this).children().length === 0) {
-    $(function() {
-    $(".peg").droppable();
-    // console.log($(this))
-  })}
-  else {
-    $(".ring").eq(0).draggable({revert: "invalid"})
-    }
-  })
+// $(".peg").mouseup(function() {
+//   if (rings.indexOf($(this).children().eq(0).attr("id")) < rings.indexOf($(this).children().first().attr("id")) || $(this).children().length === 0) {
+//     $(function() {
+//     $(".peg").droppable();
+//     // console.log($(this))
+//   })}
+//   else {
+//     $(".ring").eq(0).draggable({revert: "invalid"})
+//     }
+//   })
 // })
 // if (rings.indexOf($(this).eq(0).attr("id")) {
  // {revert: "invalid"}
