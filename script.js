52ce0539ec7c2f4dd70b696f6b6c2047a984f317
Elena Santana <fanelenan7@gmail.com>
2017-01-31 11:06:10 -0500

    logic doesn't work yet

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 $(document).ready(function(){
-
+// setting variables
 var selected = false
 var ring1 = $("#ring1")
 var ring2 = $("#ring1")
 var ring3 = $("#ring1")
-var rings = [ring1, ring2, ring3]
-// var topRing = $(".peg").find(":first-child")
+ring1 < ring2
+ring2 < ring3
+// var rings = [ring1, ring2, ring3]
+//
+// $(".peg").click(function() {
+//   console.log($(".peg").eq(0))
+//  }
+// )
+
 $(".peg").click(function() {
+  // console.log($(this))
+  // console.log($(this).children().eq(0).text())
+  // console.log($(".peg").children().eq(0).text())
+// conditional
   if (selected === true) {
-  $(this).prepend($(".selected"))
-  console.log('hi')
-  $(".selected").removeClass("selected")
-  // topRing = $(this).find(":first-child")
-  selected = false
-} else {
-  // if (jQuery.inArray(topRing, $(this))) {
-    console.log("bye")
-    $(this).find(":first-child").addClass('selected')
-    selected = true
+// logic
+    if ($(this).children().eq(0).attr("id")) >= $(".peg").children().eq(0).attr("id") || 
+  ) {
+
+    $(this).prepend($(".selected"))
+    console.log('hi')
+    $(".selected").removeClass("selected")
+    // topRing = $(this).find(":first-child")
+    selected = false
+
+  } else {
+    // if (jQuery.inArray(topRing, $(this))) {
+      console.log("bye")
+      $(this).children().eq(0).addClass('selected')
+      selected = true
   // }
 }
 })
