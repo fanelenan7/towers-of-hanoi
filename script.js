66e12e285c8bd48c6b1953385cd924f38acb58df
Elena Santana <fanelenan7@gmail.com>
2017-01-31 08:14:22 -0500

    new day commit

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
-var ring1 = $("#ring1")
-var ring2 = $("#ring2")
-var ring3 = $("#ring3")
-var rings = [ring1, ring2, ring3]
+$(document).ready(function(){
 
-$(".ring").click(function() {
-  if (($(this).css("order")) < ($(this).siblings("rings").css("order"))) {
-    console.log($(this))
-  // ($(".this").children().hasClass("div:first-child"))
-      $(".ring").eq(0).css("background-color", "rebeccapurple")
-  } else {
-    return
-    }
-  })
-// $(".peg").mouseup(function() {
-//   if (rings.indexOf($(this).children().eq(0).attr("id")) < rings.indexOf($(this).children().first().attr("id")) || $(this).children().length === 0) {
-//     $(function() {
-//     $(".peg").droppable();
-//     // console.log($(this))
-//   })}
-//   else {
-//     $(".ring").eq(0).draggable({revert: "invalid"})
-//     }
-//   })
-// })
-// if (rings.indexOf($(this).eq(0).attr("id")) {
- // {revert: "invalid"}
- // $( ".peg" ).droppable();
+var selected = false
+var pegOne = ["#ring1", "#ring2", "#ring3"]
+var pegTwo = []
+var pagThree = []
+
+$(document).click('.ring', function() {
+  if ($(this).inArray(0) {
+    console.log('yay')
+    $(this).addClass('selected')
+})
+}
+$(".peg").click(function() {
+  $(this).prepend($(".selected"))
+  $(".selected")
+})
