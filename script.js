3706c364adbe1036b0e2f26aff074198adc5f999
Elena Santana <fanelenan7@gmail.com>
2017-01-31 08:29:32 -0500

    deleted some comments, tried adding variables

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-// $(function() {
-//   $( "#sortable1, #sortable2, #sortable3" ).sortable({
-//     connectWith: ".connectedSortable"}).disableSelection()
-// })
-var rings = ["#ring1", "#ring2", "#ring3"]
-// var towers
+$(document).ready(function() {
+var rings = $(".peg").children()
+var topRing = $("peg").find(":last-child")
+
 
 $(".peg").mousedown(function() {
   if
   ($(this).children().eq(0))
-  // ($(".this").children().hasClass("div:first-child"))
    {
     // console.log($(this))
   $(function() {
     // $(this).children(".ring").eq(0)
     $(".ring").eq(0).draggable({grid: [20, 20]});
-  })} else {
+    $(".peg").droppable();
+  })
+      if ( (rings.indexOf($(this).children().eq(0).attr("id")) < rings.indexOf($(this).children().first().attr("id")) || $(this).children().length === 0) ) {
+        $(".ring").eq(0).draggable({revert: "invalid"})
+  } else {
     return
     }
-  })
-$(".peg").mouseup(function() {
-  if (rings.indexOf($(this).children().eq(0).attr("id")) < rings.indexOf($(this).children().first().attr("id")) || $(this).children().length === 0) {
-    $(function() {
-    $(".peg").droppable();
+  }
+}
+// $(".peg").mouseup(function() {
+//   if (rings.indexOf($(this).children().eq(0).attr("id")) < rings.indexOf($(this).children().first().attr("id")) || $(this).children().length === 0) {
+//     $(function() {
+    // $(".peg").droppable();
     // console.log($(this))
-  })}
-  else {
-    $(".ring").eq(0).draggable({revert: "invalid"})
-    }
-  })
+  // })}
+  // else {
+  //   $(".ring").eq(0).draggable({revert: "invalid"})
+})
+  // })
 // })
 // if (rings.indexOf($(this).eq(0).attr("id")) {
  // {revert: "invalid"}
